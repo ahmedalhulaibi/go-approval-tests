89d3c067bb07b68fa4573d32c18dd7d7c17b6dec
Ahmed Al-Hulaibi <ahmed.alhulaibi41@gmail.com>
2024-10-28 12:05:10 -0400

    tests: mark test helper

--- utils/testing_utils.go
+++ utils/testing_utils.go
@@ -5,6 +5,7 @@ import "testing"
 // AssertEqual Example:
 // AssertEqual(t, 10, number, "number")
 func AssertEqual(t *testing.T, expected, actual interface{}, message string) {
+	t.Helper()
 	if expected != actual {
 		t.Fatalf(message+"\n[expected != actual]\n[%s != %s]", expected, actual)
 	}
